e82937e07f84929edc42a9bae0eacac49e026acd
Soumya Talwar <soumya.talwar97@gmail.com>
2021-11-08 15:27:13 +0530

    markov chains with words + regex

--- bot.js
+++ bot.js
@@ -2,31 +2,50 @@ var twit = require("twit");
 var config = require("./config.js");
 var bot = twit(config);
 
-var ngrams = {};
-var n = 5;
+var wordsets = {};
+var words = [];
+// var ngrams = {};
+// var n = 3;
 var fetch = require("node-fetch");
 fetch("http://127.0.0.1:3000/text.txt")
   .then(response => response.text())
   .then(text => {
     text = text.toLowerCase();
-    for (let i = 0; i < text.length - n - 1; i++) {
-      let gram = text.substring(i, i + n);
-      if (!ngrams[gram])
-        ngrams[gram] = [];
-      ngrams[gram].push(text.charAt(i + n));
+    words = text.split(/[\s.,;:?!"\-—\d()]+/g);
+    for (let i = 0; i < words.length - 1; i++) {
+      if (!wordsets[words[i]])
+        wordsets[words[i]] = [];
+      wordsets[words[i]].push(words[i + 1]);
     }
+    // for (let i = 0; i < text.length - n - 1; i++) {
+    //   let gram = text.substring(i, i + n);
+    //   if (!ngrams[gram])
+    //     ngrams[gram] = [];
+    //   ngrams[gram].push(text.charAt(i + n));
+    // }
     setInterval(() => tweet(generate()), 5000);
   });
 
 function generate() {
-  let text = "yoong";
-  for (let i = 0; i < Math.floor(Math.random() * (280 - 100) + 100); i++) {
-    let pos = text.substring(text.length - n, text.length);
-    if (!ngrams[pos])
-      break;
-    text += ngrams[pos][Math.floor(Math.random() * ngrams[pos].length)];
-  }
-  return text;
+  let result = [words[Math.floor(Math.random() * words.length)]];
+  let length = result[0].length;
+  let limit = Math.floor(Math.random() * (280 - 100) + 100);
+  do {
+    let word = result[result.length - 1];
+    let set = wordsets[word];
+    let next = set[Math.floor(Math.random() * set.length)];
+    result.push(next);
+    length += next.length + 1;
+  } while (length < limit)
+  return (result.join(" "));
+  // let text = "yoo";
+  // for (let i = 0; i < Math.floor(Math.random() * (280 - 100) + 100); i++) {
+  //   let pos = text.substring(text.length - n, text.length);
+  //   if (!ngrams[pos])
+  //     break;
+  //   text += ngrams[pos][Math.floor(Math.random() * ngrams[pos].length)];
+  // }
+  // return text;
 }
 
 function tweet(text) {
