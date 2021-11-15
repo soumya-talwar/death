0081aeade22720d750018fbd8843c3104b1e42a9
Soumya Talwar <soumya.talwar97@gmail.com>
2021-11-15 23:51:19 +0530

    using rita markov chains

--- bot.js
+++ bot.js
@@ -1,55 +1,24 @@
 var twit = require("twit");
 var config = require("./config.js");
 var bot = twit(config);
+var RiTa = require("rita");
 
-var wordsets = {};
-var words = [];
-// var ngrams = {};
-// var n = 3;
 var fetch = require("node-fetch");
-fetch("http://127.0.0.1:3000/text.txt")
-  .then(response => response.text())
-  .then(text => {
-    text = text.toLowerCase();
-    words = text.split(/[\s.,;:?!"\-—\d()]+/g);
-    for (let i = 0; i < words.length - 1; i++) {
-      if (!wordsets[words[i]])
-        wordsets[words[i]] = [];
-      wordsets[words[i]].push(words[i + 1]);
+fetch("http://127.0.0.1:3000/tweet.json")
+  .then(response => response.json())
+  .then(dataset => {
+    let sentences = [];
+    for (let data of dataset) {
+      let text = data.tweet.full_text;
+      text = text.split(/[@#]\w+|https:\/\/t\.co\/\w+|\n+/g).join(" ").trim().toLowerCase();
+      if (!(/^rt\b/).test(text) && text.length > 0)
+        sentences.push(text);
     }
-    // for (let i = 0; i < text.length - n - 1; i++) {
-    //   let gram = text.substring(i, i + n);
-    //   if (!ngrams[gram])
-    //     ngrams[gram] = [];
-    //   ngrams[gram].push(text.charAt(i + n));
-    // }
-    setInterval(() => tweet(generate()), 5000);
-    // console.log(generate());
+    let model = new RiTa.markov(3);
+    sentences = sentences.splice(9000, 10000);
+    model.addText(sentences);
+    setInterval(() => tweet(model.generate()), 5000);
   });
-
-function generate() {
-  let result = [words[Math.floor(Math.random() * words.length)]];
-  let length = result[0].length;
-  let limit = Math.floor(Math.random() * (280 - 100) + 100);
-  while (true) {
-    let word = result[result.length - 1];
-    let set = wordsets[word];
-    let next = set[Math.floor(Math.random() * set.length)];
-    length += next.length + 1;
-    if (length - 1 > limit)
-      break;
-    result.push(next);
-  }
-  return (result.join(" "));
-  // let text = "yoo";
-  // for (let i = 0; i < Math.floor(Math.random() * (280 - 100) + 100); i++) {
-  //   let pos = text.substring(text.length - n, text.length);
-  //   if (!ngrams[pos])
-  //     break;
-  //   text += ngrams[pos][Math.floor(Math.random() * ngrams[pos].length)];
-  // }
-  // return text;
-}
 
 function tweet(text) {
   bot.post("statuses/update", {
--- test.js
+++ test.js
@@ -1,69 +1,19 @@
-var wordsets = [];
-var words = [];
-var friends = [];
+var model;
+var sentences = [];
 
-fetch("archive/data/tweet.json")
+fetch("./tweet.json")
   .then(response => response.json())
   .then(dataset => {
-    for (data of dataset) {
+    for (let data of dataset) {
       let text = data.tweet.full_text;
-      if (!(/^RT\b/).test(text)) {
-        text = text.split(/[@#]\w+\b|https:\/\/t\.co\/\w+|\n/g).join(" ").trim().toLowerCase();
-        if (text.length > 0) {
-          let text2 = text.split(/[\W\d_]+/g);
-          for (word of text2) {
-            if (word.length > 0)
-              words.push(word);
-          }
-        }
-      }
+      text = text.split(/[@#]\w+|https:\/\/t\.co\/\w+|\n+/g).join(" ").trim().toLowerCase();
+      if (!(/^rt\b/).test(text) && text.length > 0)
+        sentences.push(text);
     }
-    prepare();
-    generate();
+    model = new RiTa.markov(3);
+    sentences = sentences.splice(9000, 10000);
+    model.addText(sentences);
+    for (let i = 0; i < 10; i++) {
+      console.log(model.generate());
+    }
   });
-
-function prepare() {
-  for (let w = 1; w <= 3; w++) {
-    let wordset = {};
-    for (let i = 0; i < words.length - w; i++) {
-      let sequence = "";
-      for (let j = i; j < i + w; j++) {
-        sequence += words[j] + " ";
-      }
-      sequence = sequence.trim();
-      if (!wordset[sequence])
-        wordset[sequence] = [];
-      wordset[sequence].push(words[i + w]);
-    }
-    wordsets[w - 1] = wordset;
-  }
-  console.log(wordsets);
-}
-
-function generate() {
-  let sequences = Object.keys(wordsets[2]);
-  let phrase = sequences[Math.floor(Math.random() * sequences.length)];
-  console.log("phrase: " + phrase);
-  let limit = Math.floor(Math.random() * (280 - 100) + 100);
-  console.log("limit: " + limit);
-  while (phrase.length < limit) {
-    let current = phrase.match(/\b\w+\s\w+\s\w+\b$/g);
-    console.log("current: " + current);
-    let set = wordsets[2][current];
-    if (!set) {
-      current = phrase.match(/\b\w+\s\w+\b$/g);
-      set = wordsets[1][current];
-      if (!set) {
-        current = phrase.match(/\b\w+\b$/g);
-        set = wordsets[0][current];
-      }
-    }
-    console.log("set: " + set);
-    let next = set[Math.floor(Math.random() * set.length)];
-    console.log("next: " + next);
-    if ((phrase + " " + next).length > limit)
-      break;
-    phrase += " " + next;
-  }
-  console.log(phrase);
-}
