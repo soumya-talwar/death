2119b2c4f8ae9ab94438803de8dc6378674d0b6d
Soumya Talwar <soumya.talwar97@gmail.com>
2021-11-22 23:56:30 +0530

    testing noun exchange

--- test2.js
+++ test2.js
@@ -1,5 +1,7 @@
-var pos = {};
+var tweets = [];
 var sequences = [];
+var pos = {};
+
 fetch("http://127.0.0.1:3000/tweet.json")
   .then(response => response.json())
   .then(dataset => {
@@ -13,6 +15,7 @@ fetch("http://127.0.0.1:3000/tweet.json")
       text = text.split(/[@#]\w+|https:\/\/t\.co\/\w+|\n/g).join(" ").trim().replace(/&.+;/g, match => entities[match]);
       if (!(/^RT\b/g).test(text) && text.length > 0) {
         let words = RiTa.tokenize(text);
+        tweets.push(words);
         let tags = RiTa.pos(words);
         sequences.push(tags);
         for (let i = 0; i < tags.length; i++) {
@@ -25,15 +28,21 @@ fetch("http://127.0.0.1:3000/tweet.json")
         }
       }
     }
-    generate();
+    for (let i = 0; i < 15; i++) {
+      generate();
+    }
   });
 
 function generate() {
   let text = [];
-  let sequence = sequences[Math.floor(Math.random() * sequences.length)];
-  for (tag of sequence) {
+  let index = Math.floor(Math.random() * sequences.length);
+  let sequence = sequences[index];
+  for (let i = 0; i < sequence.length; i++) {
+    let tag = sequence[i];
     if (!pos[tag])
       text.push(tag);
+    else if (!(/^nn$/g).test(tag))
+      text.push(tweets[index][i]);
     else {
       let options = pos[tag];
       let word = options[Math.floor(Math.random() * options.length)];
