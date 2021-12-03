d1964e92806f80dd66d12afb2987bc7e692d4172
Soumya Talwar <soumya.talwar97@gmail.com>
2021-12-04 03:55:30 +0530

    menstrual + bts + rain + replies + sentiment matching

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const twit = require("twit");
 const config = require("./config.js");
 const bot = twit(config);
 
+const fetch = require("node-fetch");
 const puppeteer = require("puppeteer");
 var browser, page;
 (async () => {
@@ -14,39 +15,110 @@ var browser, page;
   });
 })();
 
-setInterval(generate, 1000 * 60);
+var moods = {
+  "excitement": {
+    "weight": 2,
+    "value": 5
+  },
+  "selfdoubt": {
+    "weight": 1,
+    "value": 2
+  }
+};
+var emojis = {
+  "happy": ["😂", "🙃", "💛", "🥺", "🌼", "💜"],
+  "sad": ["😭"]
+};
+var rain = false;
+var menstrual = false;
 
-async function generate() {
-  let result = await page.evaluate(() => {
-    return Promise.resolve(generate());
-  });
-  console.log(result);
-  // if (happy && Math.random() > 0.5)
-  //   text += emojis.happy[Math.floor(Math.random() * emojis.happy.length)];
-  // else if (!happy && Math.random() > 0.5)
-  //   text += emojis.sad[Math.floor(Math.random() * emojis.sad.length)];
-  // tweet(text);
+var bts = bot.stream("statuses/filter", {
+  follow: "335141638"
+});
+bts.on("tweet", tweet => {
+  if (tweet.user.id_str === "335141638") {
+    console.log("bts tweeted!");
+    console.log(tweet);
+    moods.excitement.value++;
+    moods.selfdoubt.value--;
+  }
+});
+
+var mention = bot.stream("statuses/filter", {
+  track: "@soumya_dead"
+});
+mention.on("tweet", tweet => {
+  let name = tweet.user.screen_name;
+  let id = tweet.id_str;
+  let quote = false;
+  if (tweet.quoted_status) {
+    if (!(/@soumya_dead/i).test(tweet.text))
+      quote = true;
+  }
+  if (!quote && !tweet.retweeted_status)
+    generate({
+      "name": name,
+      "id": id
+    });
+});
+
+setInterval(analyse, 1000 * 20);
+
+async function analyse() {
+  let date = new Date();
+  let day = date.getDate();
+  if (day > 20 && day < 28 && !menstrual) {
+    console.log("entering menstrual cycle D:");
+    moods.excitement.value--;
+    moods.selfdoubt.value++;
+    menstrual = true;
+  } else if (day > 20 && day < 28 && menstrual) {
+    console.log("exiting menstrual cycle :D");
+    moods.excitement.value++;
+    moods.selfdoubt.value--;
+    menstrual = false;
+  }
+  await fetch("https://api.openweathermap.org/data/2.5/weather?lat=13.101556425270013&lon=77.57196329497141&units=metric&appid=3d410dbee551d99b36d71387bbe879ec")
+    .then(response => response.json())
+    .then(data => {
+      let weather = data.weather[0].description;
+      if ((/rain | thunderstorm/g).test(weather) && !rain) {
+        console.log("it's raining! D:");
+        moods.excitement.value--;
+        rain = true;
+      } else if (!(/rain | thunderstorm/g).test(weather) && rain) {
+        console.log("the rain stopped! :D");
+        moods.excitement.value++;
+        rain = false;
+      }
+    });
+  generate();
 }
 
-// const fetch = require("node-fetch");
-//
-// var happy = true;
-// var emojis = {
-//   "happy": ["😂", "🙃", "💛", "🥺", "🌼", "💜"],
-//   "sad": ["😭"]
-// };
-//
-// setInterval(() => {
-//   fetch("https://api.openweathermap.org/data/2.5/weather?lat=13.101556425270013&lon=77.57196329497141&units=metric&appid=3d410dbee551d99b36d71387bbe879ec")
-//     .then(response => response.json())
-//     .then(data => {
-//       let weather = data.weather[0].description;
-//       if ((/rain | thunderstorm/g).test(weather))
-//         happy = false;
-//       else
-//         happy = true;
-//     });
-// }, 1000 * 60 * 10);
+async function generate(user) {
+  let wsum = 0;
+  let weights = 0;
+  let inverse = 1;
+  for (let mood in moods) {
+    if (mood === "selfdoubt")
+      inverse *= -1;
+    wsum += moods[mood].value * moods[mood].weight * inverse;
+    weights += moods[mood].weight;
+  }
+  let mood = wsum / weights;
+  let result = await page.evaluate((mood) => {
+    return Promise.resolve(generate(mood));
+  }, mood);
+  if (result.sentiment > 0.5 && Math.random() > 0.5)
+    result.text += " " + emojis.happy[Math.floor(Math.random() * emojis.happy.length)];
+  else if (result.sentiment < 0.5 && Math.random() > 0.5)
+    result.text += " " + emojis.sad[Math.floor(Math.random() * emojis.sad.length)];
+  console.log(result);
+  if (!user)
+    tweet(result.text);
+  else
+    reply("@" + user.name + " " + result.text, user.id);
+}
 
 function tweet(text) {
   bot.post("statuses/update", {
@@ -56,5 +128,17 @@ function tweet(text) {
       console.log("something went wrong D:");
     else
       console.log("tweet sent! :D");
+  });
+}
+
+function reply(text, tweetid) {
+  bot.post("statuses/update", {
+    in_reply_to_status_id: tweetid,
+    status: text
+  }, (error, data, response) => {
+    if (error)
+      console.log("something went wrong D:");
+    else
+      console.log("tweet sent! :D")
   });
 }
