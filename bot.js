a6bb9ea4b406e80e8e46774ee88bc4c42e0cadcc
Soumya Talwar <soumya.talwar97@gmail.com>
2021-12-13 13:44:52 +0530

    fixed puppeteer browser url

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ var browser, page;
 (async () => {
   browser = await puppeteer.launch();
   page = await browser.newPage();
-  await page.goto("http://127.0.0.1:3000/");
+  await page.goto("https://soumya-talwar.github.io/death/index.html");
   page.on("console", message => {
     let text = message.text();
     if (!(/webgl/gi).test(text))
